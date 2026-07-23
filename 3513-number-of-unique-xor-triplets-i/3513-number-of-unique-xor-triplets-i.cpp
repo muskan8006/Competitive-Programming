69d338d4a13a3d14f43bdc52a21f8d7f2654eee8
Muskan <aboutmuskann@gmail.com>
2026-07-23 21:10:13 +0530

    Time: 0 ms (100%), Space: 198.7 MB (98.13%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
-        if(n <= 2){
+        if(n <= 2)
          return n;
-        }
         int mask = 0;
         for(int num : nums){
             mask |= num;
